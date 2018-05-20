4c351ef16eae70e237a6b160bb6a57abfa80aa1f
Ethan Chung <36421238+ethanykc@users.noreply.github.com>
2018-05-20 15:24:17 -0400

    mail script change

--- scripts/mailto.js
+++ scripts/mailto.js
@@ -6,4 +6,46 @@ frmvalidator.addValidation("email","email",
   "Please enter a valid email address");
 </script>
 
+<script language="JavaScript">
+/*===========================================================*/
+/*  Contact Form
+/*===========================================================*/ 
+$('#contact-form').validate({
+rules: {
+    name: {
+        minlength: 1,
+        required: true
+    },
+    phone: {
+        minlength: 10,
+        required: false
+    },
+    email: {
+        required: true,
+        email: true
+    },
+    subject: {
+        required: true
+    },
+	website: {
+        required: false
+    },
+    message: {
+        minlength: 6,
+        required: true
+        }
+    },
+    highlight: function(element) {
+        $(element).closest('.control-group').removeClass('success').addClass('error');
+    },
+    success: function(element) {
+        element
+        .text('OK!').addClass('valid')
+        .closest('.control-group').removeClass('error').addClass('success');
+    }
+});
+	
+</script>	
+	
+	
 // JavaScript Document
